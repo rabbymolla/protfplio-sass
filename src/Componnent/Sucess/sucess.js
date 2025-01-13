a72e02e8c6734095d8b32812d5d50a44f60bf74f
M. SHOZAUL BARY <rabby16139@gmail.com>
2025-01-14 00:50:18 +0800

    font

--- src/Componnent/Sucess/sucess.js
+++ src/Componnent/Sucess/sucess.js
@@ -1,8 +1,9 @@
 "use client";
-import { DM_Sans, Open_Sans } from "next/font/google";
+
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { motion } from "framer-motion";
+import { DM_Sans, Open_Sans } from "next/font/google";
 
 const DmSans = DM_Sans({
   subsets: ["latin"],
